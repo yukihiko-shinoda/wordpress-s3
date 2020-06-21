8854a1e01b0ae00f3d1c158837e8e4871402eeac
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-06-21 21:30:37 +0900

    Limit URL fix process only when target file is text

--- config/ReplacementForFixHref.php
+++ config/ReplacementForFixHref.php
@@ -4,6 +4,14 @@ function replace_home_url($file_dest){
     if (is_dir($file_dest)) {
         return;
     }
+    $magic_file = env('WP_STATIC_PRESS_S3_MAGIC_FILE_PATH') ?: '/usr/share/misc/magic';
+    $info = file_exists($magic_file)
+        ? new FInfo(FILEINFO_MIME_TYPE, $magic_file)
+        : new FInfo(FILEINFO_MIME_TYPE);
+    $mime_type = file_exists($file_dest) ? $info->file($file_dest) : false;
+    if ( substr( $mime_type, 0, 4 ) !== 'text' ) {
+        return;
+    }
     $buff = file_get_contents($file_dest);
     if ($buff === false) {
         return;

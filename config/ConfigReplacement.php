ba40709040021d2c76bca9438dbb22807df2eb90
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-06-21 21:35:14 +0900

    Update dependencies

--- config/ConfigReplacement.php
+++ config/ConfigReplacement.php
@@ -14,11 +14,10 @@ class ConfigReplacement
     public static function replace() {
         $replacesWpSettings = [
             //↓ To read by eval()
-        '<?php'		                                                           => '',
-            //↓ To replace only StaticPress and StaticPress S3
-        'include_once( $plugin );'                                             => "if (strcmp(\$plugin, WP_CONTENT_DIR.'/plugins/staticpress2019/plugin.php') === 0) {\n        include_once(WP_CONTENT_DIR.'/plugins/staticpress2019/plugin.php');\n    } elseif (strcmp(\$plugin, WP_CONTENT_DIR.'/plugins/staticpress-s3/plugin.php') === 0) {\n        include_once(WP_CONTENT_DIR.'/plugins/staticpress-s3/plugin.php');\n    } else {\n        include_once( \$plugin );\n    }\n",
+        '<?php'		                                                               => '',
+            //↓ To replace only StaticPress
+        'include_once( $plugin );'                                                 => "if (strcmp(\$plugin, WP_CONTENT_DIR.'/plugins/staticpress2019/plugin.php') === 0) {\n        include_once(WP_CONTENT_DIR.'/plugins/staticpress2019/plugin.php');\n    } else {\n        include_once( \$plugin );\n    }\n",
         'include_once(WP_CONTENT_DIR.\'/plugins/staticpress2019/plugin.php\');'    => self::createReplacementStaticPressPlugin(),
-        'include_once(WP_CONTENT_DIR.\'/plugins/staticpress-s3/plugin.php\');' => self::createReplacementStaticPressS3Plugin(),
         ];
         $replacementWpSettings = new StrReplacement($replacesWpSettings,'ABSPATH . \'wp-settings.php\'');
         return $replacementWpSettings->renderReplacedCode();
@@ -33,25 +32,11 @@ class ConfigReplacement
             '<?php' => '',
             //↓ To read by eval()
             '__FILE__' => 'WP_CONTENT_DIR . \'/plugins/staticpress2019/plugin.php\'',
-            'require dirname( WP_CONTENT_DIR . \'/plugins/staticpress2019/plugin.php\' ) . \'/includes/class-static-press.php\';' => self::createReplacementClassStaticPress(),
+            'require_once STATIC_PRESS_PLUGIN_DIR . \'includes/class-static-press.php\';' => self::createReplacementClassStaticPress(),
             // ↓ @see http://webfood.info/staticpress-s3/#feedindexhtmlurl
-            '\'replace_relative_URI\' ), 10, 2 );' => "'replace_relative_URI' ), 10, 2 );" . ConfigReplacement::loadPHPFile(__DIR__ . '/ReplacementForFixHref.php')
+//            '\'replace_relative_uri\' ), 10, 2 );' => "'replace_relative_uri' ), 10, 2 );" . ConfigReplacement::loadPHPFile(__DIR__ . '/ReplacementForFixHref.php')
         ];
         return new StrReplacement($replacesStaticPressPluginStr, 'WP_CONTENT_DIR.\'/plugins/staticpress2019/plugin.php\'');
-    }
-
-    /**
-     * @return StrReplacement
-     */
-    public static function createReplacementStaticPressS3Plugin() {
-        $replacesStaticPressS3PluginStr = [
-            //↓ To read by eval()
-            '<?php' => '',
-            //↓ To read by eval()
-            '__FILE__' => 'WP_CONTENT_DIR.\'/plugins/staticpress-s3/plugin.php\'',
-            'require(dirname(WP_CONTENT_DIR.\'/plugins/staticpress-s3/plugin.php\').\'/includes/class-S3_helper.php\');' => self::createReplacementClassS3Helper(),
-        ];
-        return new StrReplacement($replacesStaticPressS3PluginStr, 'WP_CONTENT_DIR.\'/plugins/staticpress-s3/plugin.php\'');
     }
 
     /**
@@ -62,15 +47,7 @@ class ConfigReplacement
             //↓ To read by eval()
             '<?php' => '',
             //↓ To read by eval()
-            'str_replace( trailingslashit( ABSPATH ), trailingslashit( $this->get_site_url() ), $static_file )' => 'str_replace( trailingslashit( dirname( ABSPATH ) ), trailingslashit( $this->get_site_url() ), $static_file )',
-            //↓ To read by eval()
-            'untrailingslashit( ABSPATH )' => 'untrailingslashit( dirname( ABSPATH ) )',
-            //↓ To read by eval()
-            '__FILE__' => 'WP_CONTENT_DIR.\'/plugins/staticpress2019/includes/class-static-press.php\'',
-            //↓ To stop to dump static files under wp-admin and wp-includes directory
-            '$file_scanner->scan( trailingslashit( ABSPATH ) . \'wp-admin/\'' => '//$file_scanner->scan( trailingslashit( ABSPATH ) . \'wp-admin/\'',
-            //↓ To stop to upload static files under wp-admin and wp-includes directory to S3
-            '$file_scanner->scan( trailingslashit( ABSPATH ) . \'wp-includes/\'' => '//$file_scanner->scan( trailingslashit( ABSPATH ) . \'wp-includes/\'',
+            'require_once STATIC_PRESS_PLUGIN_DIR . \'includes/controllers/class-static-press-ajax-fetch.php\';' => self::createReplacementAjaxFetch(),
         ];
         return new StrReplacement($replacesClassStaticPressStr, 'WP_CONTENT_DIR.\'/plugins/staticpress2019/includes/class-static-press.php\'');
     }
@@ -78,16 +55,41 @@ class ConfigReplacement
     /**
      * @return StrReplacement
      */
-    public static function createReplacementClassS3Helper() {
-        $replacesClassS3HelperStr = [
+    public static function createReplacementAjaxFetch() {
+        $replacesUrlCollectorStr = [
             //↓ To read by eval()
             '<?php' => '',
-            //↓ To run on every Linux OS
-            '/usr/share/misc/magic' => WP_STATIC_PRESS_S3_MAGIC_FILE_PATH,
             //↓ To read by eval()
-            '__FILE__' => 'WP_CONTENT_DIR.\'/plugins/staticpress-s3/includes/class-S3_helper.php\'',
+            'require_once STATIC_PRESS_PLUGIN_DIR . \'includes/controllers/class-static-press-ajax-processor.php\';' => self::createReplacementAjaxProcessor(),
         ];
-        return new StrReplacement($replacesClassS3HelperStr, 'WP_CONTENT_DIR.\'/plugins/staticpress-s3/includes/class-S3_helper.php\'');
+        return new StrReplacement($replacesUrlCollectorStr, 'WP_CONTENT_DIR.\'/plugins/staticpress2019/includes/class-static-press-ajax-fetch.php\'');
+    }
+
+    /**
+     * @return StrReplacement
+     */
+    public static function createReplacementAjaxProcessor() {
+        $replacesUrlCollectorStr = [
+            //↓ To read by eval()
+            '<?php' => '',
+            //↓ To read by eval()
+            'require_once STATIC_PRESS_PLUGIN_DIR . \'includes/class-static-press-url-collector.php\';' => self::createReplacementUrlCollector(),
+        ];
+        return new StrReplacement($replacesUrlCollectorStr, 'WP_CONTENT_DIR.\'/plugins/staticpress2019/includes/class-static-press-ajax-processor.php\'');
+    }
+
+    /**
+     * @return StrReplacement
+     */
+    public static function createReplacementUrlCollector() {
+        $replacesUrlCollectorStr = [
+            //↓ To read by eval()
+            '<?php' => '',
+            //↓ To stop to dump static files under wp-admin and wp-includes directory
+            '$file_scanner_abspath->scan( \'/wp-admin/\', true ),' => '// $file_scanner_abspath->scan( \'/wp-admin/\', true ),',
+            '$file_scanner_abspath->scan( \'/wp-includes/\', true ),' => '// $file_scanner_abspath->scan( \'/wp-includes/\', true ),',
+        ];
+        return new StrReplacement($replacesUrlCollectorStr, 'WP_CONTENT_DIR.\'/plugins/staticpress2019/includes/class-static-press-url-collector.php\'');
     }
 
     /**
